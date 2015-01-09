1736014987d8da84e2c56a39be3b7dba7af5aee5
Nic Barker <nic@notivo.com>
2015-01-09 12:45:57 +1100

    Removed more unnecessary files

--- campusflora/config/routes.rb
+++ campusflora/config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'map#index'
 
-  resources :pictures
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
