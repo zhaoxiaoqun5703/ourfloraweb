546bd82e7321259b864c05382736a4c15d5bdb0f
Nic Barker <nic@notivo.com>
2015-01-16 09:03:38 +1100

    Changed production picture url

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,11 @@
 class Picture < ActiveRecord::Base
   has_many :species_pictures
   has_many :species, through: :species_pictures
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture,
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png",
+                    :path => ":rails_root/app/assets/images/species_images/:id/:style_:basename.:extension",
+                    :url => "/app/assets/images/species_images/:id/:style_:basename.:extension"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
   def picture_url
