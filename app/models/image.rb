b24f9a31f979e32609f33a172778fb4a5acd6d4b
Nic Barker <nic@notivo.com>
2015-01-16 11:53:52 +1100

    Another update to paperclip path

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,7 +9,7 @@ class Image < ActiveRecord::Base
   has_attached_file :image,
                     :styles => { :medium => "300x300>",
                     :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-                    :path => ":rails_root/app/assets/images/species_images/:species_name/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/assets/species_images/:species_name/:id/:style_:basename.:extension",
                     :url  => "/assets/species_images/:species_name/:id/:style_:basename.:extension"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
