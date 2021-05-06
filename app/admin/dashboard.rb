23970d3e3ac9428a83d5c39466849c1b476dd6e8
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-06 18:26:36 +0800

    Update dashboard.rb

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,19 +1,20 @@
 ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-  form do |f|
-    f.inputs 'Details' do
-      f.input :title
-      f.input :published_at, label: 'Publish Post At'
-    end
-    f.actions
-  end
+  
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
+    end
+    form do |f|
+      f.inputs 'Details' do
+        f.input :title
+        f.input :published_at, label: 'Publish Post At'
+      end
+      f.actions
     end
 
       # div class: "blank_slate_container", id: "dashboard_default_message" do
