752b99b49049f78c9cf7f0dc951f1daa97bcde95
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-06 18:19:17 +0800

    Update dashboard.rb

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,14 +9,14 @@ ActiveAdmin.register_page "Dashboard" do
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-    show do 
+
       div class: "blank_slate_container", id: "dashboard_default_message" do
         span class: "blank_slate" do
           span I18n.t("hello")
           # small I18n.t("active_admin.dashboard_welcome.call_to_action")
         end
       end
-    end
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
