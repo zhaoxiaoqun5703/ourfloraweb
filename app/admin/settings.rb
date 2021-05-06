7012dbfdc4eddc0211e434a301f45a3c74f4ed0d
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-06 17:05:01 +0800

    Update settings.rb

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,28 +1,11 @@
 ActiveAdmin.register Settings do
-    permit_params :email, :password, :password_confirmation
-    
-    index do
-      selectable_column
-      id_column
-      column :email
-      column :current_sign_in_at
-      column :sign_in_count
-      column :created_at
-      actions
-    end
-  
-    filter :email
-    filter :current_sign_in_at
-    filter :sign_in_count
-    filter :created_at
-  
-    form do |f|
-      f.inputs "Admin Details" do
-        f.input :email
-        f.input :password
-        f.input :password_confirmation
-      end
-      f.actions
+    content title: proc{ I18n.t("active_admin.dashboard") } do
+        div class: "blank_slate_container", id: "dashboard_default_message" do
+          span class: "blank_slate" do
+            span I18n.t("active_admin.dashboard_welcome.welcome")
+            small I18n.t("active_admin.dashboard_welcome.call_to_action")
+          end
+        end
     end
   
   end
