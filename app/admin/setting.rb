a2a22b011e71e76b13cc05becc01c61b2c32b87b
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 20:05:49 +0800

    Update setting.rb

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -1,28 +1,28 @@
 ActiveAdmin.register_page "Setting" do
   # permit_params :about_page_content
-  actions :all, :except => [:destroy]
+  # actions :all, :except => [:destroy]
 
-  # Prevent a new resource from being created, we can only edit this one row
-  config.clear_action_items!
+  # # Prevent a new resource from being created, we can only edit this one row
+  # config.clear_action_items!
 
-  show do |setting|
-    # Initialize a markdown parser
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
-    attributes_table do
-      row :about_page_content do
-        markdown.render(setting.about_page_content).html_safe
-      end
-    end
+  # show do |setting|
+  #   # Initialize a markdown parser
+  #   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+  #   attributes_table do
+  #     row :about_page_content do
+  #       markdown.render(setting.about_page_content).html_safe
+  #     end
+  #   end
 
-    active_admin_comments_for(resource)
-  end
+  #   active_admin_comments_for(resource)
+  # end
 
-    # This defines the column order for the index page which shows the list of all species
-  index do
-    column :about_page_content
-    column :created_at
-    column :updated_at
-    actions
-  end
+  #   # This defines the column order for the index page which shows the list of all species
+  # index do
+  #   column :about_page_content
+  #   column :created_at
+  #   column :updated_at
+  #   actions
+  # end
 
 end
