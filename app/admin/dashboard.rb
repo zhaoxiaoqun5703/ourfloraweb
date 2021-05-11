f5844641a15f31d49bc8414fc3f60022483732ee
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 16:43:17 +0800

    Update dashboard.rb

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,15 +9,15 @@ ActiveAdmin.register_page "Dashboard" do
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-    form do |f|
-      f.inputs do
-        f.input :name
-        f.input :email
-        f.input :birthday
-        f.input :foobar, :as => :string
-      end
-      f.buttons
-    end
+    # form do |f|
+    #   f.inputs do
+    #     f.input :name
+    #     f.input :email
+    #     f.input :birthday
+    #     f.input :foobar, :as => :string
+    #   end
+    #   f.buttons
+    # end
 
       # div class: "blank_slate_container", id: "dashboard_default_message" do
       #   span class: "blank_slate" do
