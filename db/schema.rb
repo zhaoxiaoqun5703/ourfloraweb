88052b9273695ee6b7ad2e6fe2402451eb19b398
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 17:39:21 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,23 +47,11 @@ ActiveRecord::Schema.define(version: 20170406052832) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 #copy
   create_table "settings", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text     "about_page_content", limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
-
-  add_index "settings", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "settings", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-#end copy
+ #end copy
   create_table "families", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
