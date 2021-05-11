b9e0a55e4f4089fdc8ddd863013cd86d91d09b2a
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 20:29:19 +0800

    Update settings_controller.rb

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,7 +3,7 @@ class SettingsController < InheritedResources::Base
   private
 
     def setting_params
-      params.require(:setting).permit(:about_page_content)
+      params.required(:setting).permit(:about_page_content)
     end
 end
 
