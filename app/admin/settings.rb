b5086c58fd99383fe38fa0c950116b5474212f24
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 17:07:40 +0800

    Update settings.rb

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,21 +1,21 @@
 ActiveAdmin.register_page "Settings" do
   permit_params :longitude, :latitude, :zoom
 
-  index do
-    selectable_column
-    id_column
-    column :longitude
-    column :latitude
-    column :zoom
-    actions
-  end
+  # index do
+  #   selectable_column
+  #   id_column
+  #   column :longitude
+  #   column :latitude
+  #   column :zoom
+  #   actions
+  # end
 
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :longitude
-      f.input :latitude
-      f.input :zoom
-    end
-    f.actions
-  end
+  # form do |f|
+  #   f.inputs "Admin Details" do
+  #     f.input :longitude
+  #     f.input :latitude
+  #     f.input :zoom
+  #   end
+  #   f.actions
+  # end
 end
