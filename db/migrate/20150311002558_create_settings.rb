b4705381f4a078dcb53dae83395893af9c9ccd43
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-11 18:05:53 +0800

    Create 20150311002558_create_settings.rb

---
+++ db/migrate/20150311002558_create_settings.rb
@@ -0,0 +1,11 @@
+class CreatePageContent < ActiveRecord::Migration
+  def change
+    create_table :settings do |t|
+      t.text :about_page_content
+      t.timestamps null: false
+    end
+
+    # Create a single row for the about page content
+    PageContent.create!(:about_page_content => 'Test Content')
+  end
+end 
